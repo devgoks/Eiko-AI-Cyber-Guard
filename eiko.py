8eb1e6ec6910c6a7e704962468d797633df957be
Olamigoke Olabampe <goke@qrios.com>
2024-09-25 16:06:10 +0100

    Added Google Gemini as Second Detector within the solution

--- eiko.py
+++ eiko.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+import google.generativeai as genai
 
 load_dotenv()
 
@@ -9,11 +10,15 @@ load_dotenv()
 openai_api_key = os.environ.get('OPENAPI_SECRET_KEY')
 client = OpenAI(api_key=openai_api_key)
 
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
 app = Flask(__name__)
 
 def check_fraudulent_message(message):
+    fraud_check_instruction = "Is the following message fraudulent?..., always include an advice in your response.....Also, at the end of your response if fraudulent add hashtag #Fraudulent if not #Nonfraudulent"
+
     messages = [
-        {"role": "system", "content": "Is the following message fraudulent?..., always include an advice in your response.....Also, at the end of your response if fraudulent add hashtag #Fraudulent if not #Nonfraudulent"},
+        {"role": "system", "content": fraud_check_instruction},
         {"role": "user", "content": message}
     ]
 
@@ -22,12 +27,17 @@ def check_fraudulent_message(message):
         messages=messages
     )
 
-    assistant_response = response.choices[0].message.content
+    chatgpt_assistant_response = response.choices[0].message.content
 
-    if '#fraudulent' in assistant_response.lower():
-        return True, assistant_response
-    else:
-        return False, assistant_response
+    print(response)
+
+    google_model = genai.GenerativeModel("gemini-1.5-flash")
+
+    google_model_response = google_model.generate_content(fraud_check_instruction + " \n Text to Check: \n"+message)
+
+    print(google_model_response.text)
+
+    return chatgpt_assistant_response, google_model_response.text
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -37,8 +47,20 @@ def index():
         sender = request.form["sender"]
         time_received = request.form["time_received"]
         message = "channel: " + channel + ", sender: " + sender + ", time_received: " + time_received + ", message: " + user_input
-        fraudulent, advice = check_fraudulent_message(message)
-        return render_template("result.html", user_input=user_input, fraudulent=fraudulent, advice=advice)
+        chatgpt_advice, gemini_advice = check_fraudulent_message(message)
+
+        if '#fraudulent' in chatgpt_advice.lower():
+            chatgpt_fraudulent = True
+        else:
+            chatgpt_fraudulent = False
+        if '#fraudulent' in gemini_advice.lower():
+            gemini_fraudulent = True
+        else:
+            gemini_fraudulent = False
+
+        return render_template("result.html", user_input=user_input, gemini_advice=gemini_advice,
+                               gemini_fraudulent=gemini_fraudulent,
+                               chatgpt_fraudulent=chatgpt_fraudulent, chatgpt_advice=chatgpt_advice)
     return render_template("index.html")
 
 if __name__ == "__main__":
